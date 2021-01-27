f6c6529e6f852a68f5a81dad2721f7de0c27ff3f
Vishal Chauhan <8529097+vishal0143@users.noreply.github.com>
2021-01-28 01:44:33 +0530

    Create plot_history.py

---
+++ plot_history.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+def plot_metrics(metric_name, title, ylim=5):
+  plt.title(title)
+  plt.ylim(0,ylim)
+  plt.plot(history.history[metric_name], color='blue',label = metric_name)
+  plt.plot(history.history['val_'+metric_name],
+                           color= 'green',
+                           label = 'val_'+metric_name)
